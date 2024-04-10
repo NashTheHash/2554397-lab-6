3ddb6ad8ca6a1ff5c494a65f63f4ebabe0697e0b
NashTheHash <2554397@students.wits.ac.za>
2024-04-10 21:43:19 +0200

    Fixed spelling mistakes

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ carForm.addEventListener('submit', event => {
 // Function to remove a car
 function removeCar(index) {
     const carId = cars[index].id;
-    fetch(`https://lucky-lot-api.azurewebsites.net/ars/${carId}`, {
+    fetch(`https://lucky-lot-api.azurewebsites.net/cars/${carId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
